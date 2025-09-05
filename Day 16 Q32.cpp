8c2996319b1e56a4e7a37f84a61173bf5eb3e73d
payalrathod-enthu <payal.28512@stu.upes.ac.in>
2025-09-05 22:14:41 +0530

    Added Day 16 Question 32 Solution

---
+++ Day 16 Q32.cpp
@@ -0,0 +1,31 @@
+/*
+Q: Write a program to check if a number is a palindrome.
+   Example: Input = 121 ? Palindrome
+            Input = 123 ? Not a Palindrome
+*/
+
+#include <stdio.h>
+
+int main() {
+    int num, original, reversed = 0, remainder;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    original = num;  
+
+    while (num != 0) {
+        remainder = num % 10;              
+        reversed = reversed * 10 + remainder; 
+        num /= 10;
+    }
+
+    if (original == reversed) {
+        printf("%d is a Palindrome.\n", original);
+    } else {
+        printf("%d is Not a Palindrome.\n", original);
+    }
+
+    return 0;
+}
+
