394c5fc9e08185a25364307534719a25ecbb277f
payalrathod-enthu <payal.28512@stu.upes.ac.in>
2025-09-08 14:14:49 +0530

    Added Day 19 Question 37 Solution

---
+++ Day 19 Q37.cpp
@@ -0,0 +1,30 @@
+/*
+Q: Write a program to find the LCM of two numbers.
+   Using HCF (Euclidean Algorithm).
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a, b, x, y, temp, hcf, lcm;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    x = a;
+    y = b;
+
+    while (b != 0) {
+        temp = b;
+        b = a % b;
+        a = temp;
+    }
+    hcf = a;
+
+    lcm = (x * y) / hcf;
+
+    printf("LCM = %d\n", lcm);
+
+    return 0;
+}
+
