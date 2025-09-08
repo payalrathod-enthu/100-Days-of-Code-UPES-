0cea51b53dc36c9e3baa53d458054b0e1ee485fb
payalrathod-enthu <payal.28512@stu.upes.ac.in>
2025-09-08 14:13:38 +0530

    Added Day 18 Question 35 Solution

---
+++ Day 18 Q35.cpp
@@ -0,0 +1,25 @@
+/*
+Q: Write a program to print all factors of a given number.
+   Factors are numbers that divide the given number completely.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int num, i;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Factors of %d are: ", num);
+
+    for (i = 1; i <= num; i++) {
+        if (num % i == 0) {   
+            printf("%d ", i);
+        }
+    }
+
+    printf("\n");
+    return 0;
+}
+
