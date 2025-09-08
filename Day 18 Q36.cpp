9ae4f64570625f20a9d17daeb2c117cf4e9e3d80
payalrathod-enthu <payal.28512@stu.upes.ac.in>
2025-09-08 14:14:14 +0530

    Added Day 18 Question 36 Solution

---
+++ Day 18 Q36.cpp
@@ -0,0 +1,24 @@
+/*
+Q: Write a program to find the HCF (GCD) of two numbers.
+   Using Euclidean Algorithm.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    while (b != 0) {
+        temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    printf("HCF (GCD) = %d\n", a);
+
+    return 0;
+}
+
