7b1e93d6ba59543dde70cf19b2ab081e58925a39
payalrathod-enthu <payal.28512@stu.upes.ac.in>
2025-09-26 20:16:50 +0530

    Update and rename Day 20 Q40.cpp to Day20_B.c

--- Day20_B.c
+++ Day20_B.c
@@ -1,5 +1,5 @@
 /*
-Q: Write a program to find the 1’s complement of a binary number and print it.
+Q: Write a program to find the 1â€™s complement of a binary number and print it.
 */
 
 #include <stdio.h>
